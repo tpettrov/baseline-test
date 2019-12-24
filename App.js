662e78f7981badf1457cc826e43319c0de2e0ace
Anton <anton.petrov@athlonsofia.com>
2019-12-24 17:08:27 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Text>Constant, K!</Text>
+    </>
+  );
+};
+
+export default App;
